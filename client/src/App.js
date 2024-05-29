2892e03d5718d77209e8624943b6a48af16516f3
Angelo <herman256561@gmail.com>
2024-05-29 16:19:31 +0800

    removed .git from client service

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
+
+const App = () => {
+    return <div className='container'>
+        <h1>Post Create</h1>
+        <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />
+    </div>;
+  };
+   
+  export default App;
---
+++ client/src/CommentList.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const CommentList = ({comments}) =>{
+
+    const renderedComments = comments.map(comment => {
+        let content = comment.content;
+
+        if(comment.status==='approved'){
+            content = comment.content;
+        }
+        if(comment.status==='pending'){
+            content = "This comment has not been moderated.";
+        }
+        if(comment.status==='rejected'){
+            content = "This comment has been rejected";
+        }
+
+        return <li key={comment.id}>{content}</li>
+    });
+
+    return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
